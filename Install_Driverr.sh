68cc98d7fd3ecb675beee9d8655f6cde6f11fc0b
Sagandeep-kaur <115706027+Sagandeep-kaur@users.noreply.github.com>
2024-11-27 15:26:16 +0530

    Add files via upload

---
+++ Install_Driverr.sh
@@ -0,0 +1,20 @@
+yum update
+ex /etc/yum.repos.d/google-chrome.repo << END
+i
+[google-chrome]
+name=google-chrome
+baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
+enabled=1
+gpgcheck=1
+gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub
+.
+wq
+END
+#vi /etc/yum.repos.d/google-chrome.repo
+#[google-chrome]
+#name=google-chrome
+#baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
+#enabled=1
+#gpgcheck=1
+#gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub
+yum install google-chrome-stable
