bf5ccc349c98cf8ec1808aaed7f9d3189df6975a
Sagandeep-kaur <115706027+Sagandeep-kaur@users.noreply.github.com>
2024-11-28 13:41:52 +0530

    Update SeleniumScript_Chrome.py

--- SeleniumScript_Chrome.py
+++ SeleniumScript_Chrome.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-# Create a new instance of the Firefox driver
+# Create a new instance of the Chrome driver
 
-driver = webdriver.Chrome()
+options = Options()
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(executable_path=r'/usr/local/bin/chromedriver', chrome_options=options)
 
 # Navigate to a website
 driver.get("https://www.techlistic.com/p/selenium-practice-form.html")
