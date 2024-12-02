a8d8f1a916d95040d5d2d9b804caa09f23a90d61
Sagandeep-kaur <115706027+Sagandeep-kaur@users.noreply.github.com>
2024-12-02 18:18:57 +0530

    Update Selenium_script_chromedriver.py

--- Selenium_script_chromedriver.py
+++ Selenium_script_chromedriver.py
@@ -11,25 +11,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-#from webdriver_manager.chrome import ChromeDriverManager
+
 
 # Create a new instance of the Chrome driver
 
-"""
-# options.add_argument('--no-sandbox')
-# options.add_argument('--disable-dev-shm-usage')
-#service = Service(executable_path = r'C:/Users/sagandeepk/Downloads/chromedriver-linux64/chromedriver')
-#options = webdriver.ChromeOptions()
-#driver = webdriver.Chrome(service=service, options=options)
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")  # Required for some environments (e.g., CI)
+options.add_argument("--disable-dev-shm-usage")  # Required for some environments (
 
-
-#chrome_install = ChromeDriverManager().install()
-#folder = os.path.dirname(chrome_install)
-#chromedriver_path = os.path.join(folder, "chromedriver.exe")
-#service = webdriver.ChromeService(chromedriver_path)
-"""
-
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 
 # Navigate to a website
@@ -38,5 +29,12 @@ driver.get("https://www.techlistic.com/p/selenium-practice-form.html")
 element = driver.find_element(By.XPATH,"//input[@type='text']")
 # Perform actions on the element
 element.send_keys('Sagandeep')
+print(element.text)
+links=driver.find_elements(By.TAG_NAME,'a')
+
+print(len(links))
+for link in links:
+    print(link.get_attribute('href'))
+
 # Close the browser
 driver.quit()
