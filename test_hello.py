fec5a59302ba4120d18f1a81bc2fb6f4d336d575
Sagandeep-kaur <115706027+Sagandeep-kaur@users.noreply.github.com>
2025-03-02 17:35:07 +0530

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,21 +1,21 @@
 def test_func():
-{ 
-  print('hello')
-  print("2nd line via git cli")
-  print('3rd line via gui')
-  print('4th line via git cli')
-  print('5th line via gui')
-  print('7th line via cli')
-  print('6th line via gui')
-  print('8th line via gui')
-  print('9th line via cli')
-  print('editing via cli')
-  print('10th line via gui')
-  print('via master branch')
-  print('editing from another branch')
-  print('editing again from new branch')
-  print('edit from main')
-}
+ 
+   print('hello')
+   print("2nd line via git cli")
+   print('3rd line via gui')
+   print('4th line via git cli')
+   print('5th line via gui')
+   print('7th line via cli')
+   print('6th line via gui')
+   print('8th line via gui')
+   print('9th line via cli')
+   print('editing via cli')
+   print('10th line via gui')
+   print('via master branch')
+   print('editing from another branch')
+   print('editing again from new branch')
+   print('edit from main')
+  
 
 test_func()
 
